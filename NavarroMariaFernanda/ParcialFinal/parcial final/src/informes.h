6f9fc9e0103dbc65aaa40d0cdf3bf9c3eeefdb0e
femavana <femavana@gmail.com>
2019-10-23 17:09:02 -0300

    Update informes.h

--- NavarroMariaFernanda/ParcialFinal/parcial final/src/informes.h
+++ NavarroMariaFernanda/ParcialFinal/parcial final/src/informes.h
@@ -21,21 +21,21 @@ int orderListPending(Pedidos* pedido,Clients* client,int cant,int size);
 * \param cant int size for client
 * \param size int size for pedido
 */
-int listClients(Pedidos* pedido,Clients* client,int cant, int size);
+int listClients(Pedidos* pedido,Clients* client,int cant,int size);
 /** \brief Muestra la cantidad de clientes que reciclaron mas de 1000Kg
  * \param pedido list pedidos*
 * \param client list Clients*
 * \param cant int size for client
 * \param size int size for pedido
 */
-int kilosRecicladosMayorA(Pedidos* pedido,Clients* client,int cant, int size);
+int kilosRecicladosMayorA(Pedidos* pedido,Clients* client,int cant,int size);
 /** \brief Muestra la cantidad de clientes que reciclaron menos de 100Kg
  * \param pedido list pedidos*
 * \param client list Clients*
 * \param cant int size for client
 * \param size int size for pedido
 */
-int kilosRecicladosMenorA(Pedidos* pedido,Clients* client,int cant, int size);
+int kilosRecicladosMenorA(Pedidos* pedido,Clients* client,int cant,int size);
 /** \brief Carga las operaciones del MenuInformes
  * \param pedido list pedidos*
 * \param client list Clients*
@@ -60,35 +60,42 @@ int listPendingByLocation(Pedidos* pedido,Clients* client,int cant,int size);
 * \param cant int size for client
 * \param size int size for pedido
 */
-int clientsMostOrders(Pedidos* pedido,Clients* client,int cant, int size);
+int clientsMostOrders(Pedidos* pedido,Clients* client,int cant,int size);
 /** \brief Muestra el cliente con mas pedidos pendientes
  * \param pedido list pedidos*
 * \param client list Clients*
 * \param cant int size for client
 * \param size int size for pedido
 */
-int clientsMostOrdersPending(Pedidos* pedido,Clients* client,int cant, int size);
+int clientsMostOrdersPending(Pedidos* pedido,Clients* client,int cant,int size);
 /** \brief Muestra el cliente con mas pedidos completados
  * \param pedido list pedidos*
 * \param client list Clients*
 * \param cant int size for client
 * \param size int size for pedido
 */
-int clientsMostOrdersProcesed(Pedidos* pedido,Clients* client,int cant, int size);
+int clientsMostOrdersProcesed(Pedidos* pedido,Clients* client,int cant,int size);
 /** \brief Muestra el cliente con mas kilos procesados
  * \param pedido list pedidos*
 * \param client list Clients*
 * \param cant int size for client
 * \param size int size for pedido
 */
-int clientsMostRecicled(Pedidos* pedido,Clients* client,int cant, int size);
+int clientsMostRecicled(Pedidos* pedido,Clients* client,int cant,int size);
 /** \brief Muestra el cliente con menos kilos procesados
  * \param pedido list pedidos*
 * \param client list Clients*
 * \param cant int size for client
 * \param size int size for pedido
 */
-int clientsLessRecicled(Pedidos* pedido,Clients* client,int cant, int size);
+int clientsLessRecicled(Pedidos* pedido,Clients* client,int cant,int size);
+/** \brief Recibe un tipo de plastico y cuit y muestra la cantidad de kilos reciclados
+ * \param pedido list pedidos*
+* \param client list Clients*
+* \param cant int size for client
+* \param size int size for pedido
+*/
+int kilosRecicladoByTipoPlastico(Pedidos* pedido,Clients* client,int cant,int size);
 /** \brief Lista los pedidos completados y muestra el porcentaje de reciclado de cada uno
  * \param pedido list pedidos*
 * \param client list Clients*
@@ -100,3 +107,4 @@ int orderProcessedListByCriteria(Pedidos* pedido,Clients* client,int cant,int si
 
 
 #endif //  INFORMES_H_INCLUDED
+
